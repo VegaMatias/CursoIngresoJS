559d8f95c69c324e142f74735efdb9078e9c06f3
alumno <alumno>
2019-02-14 19:45:58 -0300

    ejercicio 1 corregidoDos

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,12 +1,17 @@
 
 function mostrar()
 {
-	  alert("Tenes dos mascotas xxxx y xxxxx, que pesan xx y xx kilos, la suma de los kilos es xx");
+	//  alert("Tenes dos mascotas xxxx y xxxxx, que pesan xx y xx kilos, la suma de los kilos es xx");
 
 
-	  var mascotaUno;
-	  var mascotaDos;
+	  var nombreUno;
+	  var nombreDos;
+	  var pesoUno;
+	  var pesoDos;
 	  var suma;
+
+	  nombreUno = prompt("Ingresa el nombre de su primera mascota: ");
+	  nombreDos = prompt("Ingresa el nombre de su segunda mascota: ");
 
 	  mascotaUno = prompt("Ingresa cuanto pesa la primera mascota: ");
 	  mascotaDos = prompt("Ingresa cuanto pesa la segunda mascota: ");
@@ -16,5 +21,10 @@ function mostrar()
 
 	  suma = mascotaUno + mascotaDos;
 
-	  alert("Las dos mascotas en total suman: " + suma);
+//alert("Tenes dos mascotas xxxx y xxxxx, que pesan xx y xx kilos, la suma de los kilos es xx");
+
+//en que se diferencian??
+
+	  alert("Tenes dos mascotas " + nombreUno + " y " + nombreDos + ", que pesan " + mascotaUno + " y " + mascotaDos + " kilos, la suma de los kilos es " + suma );
+
 }
