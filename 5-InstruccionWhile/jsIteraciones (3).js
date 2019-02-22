54feee392c95e60c8c3b59b1c312f6acb60f2480
alumno <alumno>
2019-02-21 21:56:51 -0300

    while

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,30 @@
 function mostrar()
 {
+/*
+mientras no sea!!
+no es si no se ue..
+es MIENTRAS LA CLAVE SEA DISTINTA A "UTN750"
+	while(clave!= "utn750")
 
-var clave = prompt("ingrese el número clave.");
+	hasta aca es bucle infinito... 
 
+	hasta que (el punto 3 )
+	que es pedir la clave de vuelta...
+
+	clave = pro..("error, ingrese el numero clave")
+
+bloquea.. el while.. mientras nos e cumpla.. no para
+*/
+	var clave 
+	clave = prompt("ingrese el número clave.");
+
+	while(clave != "UTN750")
+	{
+
+
+		clave = prompt("ERROR, ingrese el numero clave.");
+	}
+
+	alert("Bienvenido!");
 
 }//FIN DE LA FUNCIÓN
