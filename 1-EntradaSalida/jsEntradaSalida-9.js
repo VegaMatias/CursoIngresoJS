380235470f81e68dcaa5a6870bd327c74dc3182d
VegaMatias <47436526+VegaMatias@users.noreply.github.com>
2019-07-02 21:50:16 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,16 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+    var importe;
+    var resultado;
+
+    importe = document.getElementById("sueldo").value;
+
+    importe = parseInt(importe);
+
+    resultado = importe + (importe * 10 / 100);
+
+    document.getElementById("resultado").value = resultado;
 
 	
 }
